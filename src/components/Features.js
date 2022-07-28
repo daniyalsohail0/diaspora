7c572610921fff65424aa6b4f76eafcea8cceff4
daniyalsohail0 <daniyalsohail0@gmail.com>
2022-07-28 05:54:38 +0500

    chaange

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import {AiOutlineArrowRight} from 'react-icons/ai'
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className='flex justify-center items-center'>
+      <div className='md:w-1/2 flex flex-col justify-center items-center'>
+        <h1 className="text-yellow-400 md:text-5xl text-2xl text-center font-extrabold m-2 p-2 italic uppercase">
+          more than just a coin
+        </h1>
+        <h1 className='text-white md:text-6xl text-3xl text-center font-bold md:py-16 p-4'>
+          Subscribe today and stay up-to-date to all important announcements
+        </h1>
+        <p className='text-white text-center md:p-8 p-4 md:w-2/3'>Deliver the same engaging experience and networking opportunities whether your audience attends virtually or in-person. Contact our team to set up a demo of our platform.</p>
+        <button className='flex font-semibold text-black gap-1 tracking-wide bg-gradient-to-l from-yellow-300 to-yellow-600 hover:bg-gradient-to-r hover:shadow-md hover:shadow-blue-800 duration-100 ease-in rounded px-2 py-2 md:mx-10 my-2 md:my-0'>
+            <span>Subscribe</span>
+            <AiOutlineArrowRight className='text-2xl'/>
+        </button>
+      </div>
+    </div>
   )
 }
 
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import {BsWallet} from 'react-icons/bs'
 
 const HeroSection = () => {
   return (
     <div>
       <div className='flex flex-col justify-center items-center m-4 p-4 h-screen'>
         <p className='text-white m-2 p-2'>Welcome to Diaspora</p>
-        <h1 className='text-white text-5xl text-center font-extrabold m-2 p-2 md:w-1/3'>The Future Of Crypto Is Here!</h1>
-        <p className='text-white text-lg text-center md:w-1/3 m-2 p-2'>Diaspora is backed by iVOBS which is the new cutting-edge state-of-the-art marketplace.</p>
-        <button className='flex font-semibold text-black tracking-wide bg-gradient-to-l from-yellow-300 to-yellow-600 hover:bg-gradient-to-r hover:shadow-md hover:shadow-blue-800 duration-100 ease-in rounded px-2 py-2 md:mx-10 my-2 md:my-0'>
-            Connect Wallet
+        <h1 className='text-white md:text-5xl text-4xl text-center font-extrabold m-2 p-2 md:w-3/4 text-yellow-400 uppercase'>The Future Of Crypto Is Here!</h1>
+        <p className='text-white md:text-lg text-center md:w-1/3 m-2 p-2'>Diaspora is backed by iVOBS which is the new cutting-edge state-of-the-art marketplace.</p>
+        <button className='flex font-semibold gap-1 text-black tracking-wide bg-gradient-to-l from-yellow-300 to-yellow-600 hover:bg-gradient-to-r hover:shadow-md hover:shadow-blue-800 duration-100 ease-in rounded px-2 py-2 md:mx-10 my-2 md:my-0'>
+            <span>Connect Wallet</span>
+            <BsWallet className='text-2xl' />
         </button>
       </div>
     </div>
